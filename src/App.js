361aacc55a1253619aeeb268ae0e2a53548ae5e2
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-11-15 23:23:55 -0800

    installed react router and redux form, logger, and thunk

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  render () {
+    const array = [1, 2, 3];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React!
           </a>
         </header>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
