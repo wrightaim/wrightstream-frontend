8e5d31a94c904106708985348bc4966e2e469850
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-11-26 21:13:25 -0800

    set up initial app, header, and login components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+// REACT
 import React from 'react';
+
+// ROUTER
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+// REDUX
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+//import { getUser } from './state/actions/auth';
+
+// COMPONENTS
+import Header from './components/Header';
+
+// CONTAINERS
+import Login from './containers/Login';
+
+// ==========
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        hi
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={() => <Redirect to="/login" />} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </BrowserRouter>  
     );
   };
 };
+
+// const mapStateToProps = state => ({
+//   user: state.auth.user,
+//   authorized: state.auth.authorized
+// });
+
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//   getUser
+// }, dispatch);
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default App;
