8d43232e43e46c4d472ce8c5ae3e8341e108507f
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-11-26 20:01:31 -0800

    removed js-watch from package.json scripts

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import React from 'react';
 
 class App extends React.Component {
   render () {
-    const array = [1, 2, 3];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!
-          </a>
-        </header>
+      <div>
+        hi
       </div>
     );
   };
