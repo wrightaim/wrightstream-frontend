d1cf001018fd52cfbb5f1243e9eb5b29fea97dc6
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-11-30 01:49:22 -0800

    added styling to login form

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -30,57 +30,61 @@ class Login extends React.Component {
           <div className="container">
             <div className="columns">
               <div className="column is-4-desktop is-offset-4-desktop">
-                <form onSubmit={this.handleLogin}>
-                  <div className="field">
-                    <p className="control">
-                      <input
-                        className="input"
-                        type="text"
-                        placeholder="Shop Username"
-                        id="shop_username"
-                        value={this.state.shop_username}
-                        onChange={event => this.setState({shop_username: event.target.value})}
-                        required
-                      />
-                    </p>
+                <div className="card">
+                  <div className="card-content">
+                    <form onSubmit={this.handleLogin}>
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="text"
+                            placeholder="Shop Username"
+                            id="shop_username"
+                            value={this.state.shop_username}
+                            onChange={event => this.setState({shop_username: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="email"
+                            placeholder="Email"
+                            id="email"
+                            value={this.state.email}
+                            onChange={event => this.setState({email: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="password"
+                            placeholder="Password"
+                            id="password"
+                            value={this.state.password}
+                            onChange={event => this.setState({password: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                      <div className="control has-text-centered">
+                        <button className="button is-main-button">Log In</button>
+                      </div>
+                      {
+                        this.props.showLoginError ? (
+                          <p id="error" className="help is-danger">
+                            Email or password is incorrect.
+                          </p>
+                        ) : null
+                      }
+                    </form>
                   </div>
-                  <div className="field">
-                    <p className="control">
-                      <input
-                        className="input"
-                        type="email"
-                        placeholder="Email"
-                        id="email"
-                        value={this.state.email}
-                        onChange={event => this.setState({email: event.target.value})}
-                        required
-                      />
-                    </p>
-                  </div>
-                  <div className="field">
-                    <p className="control">
-                      <input
-                        className="input"
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        value={this.state.password}
-                        onChange={event => this.setState({password: event.target.value})}
-                        required
-                      />
-                    </p>
-                  </div>
-                  <div className="control">
-                    <button className="button is-primary is-fullwidth">Log In</button>
-                  </div>
-                  {
-                    this.props.showLoginError ? (
-                      <p id="error" className="help is-danger">
-                        Email or password is incorrect.
-                      </p>
-                    ) : null
-                  }
-                </form>
+                </div>
               </div>
             </div>
           </div>
