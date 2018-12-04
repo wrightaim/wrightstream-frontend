5cbb4fb901fbdef14839cc4876bbacbf5b83e3c0
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-03 20:39:13 -0800

    added sass mixin for minimum full height and vertical align center

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -25,71 +25,73 @@ class Login extends React.Component {
 
   render () {
     return (
-      <section className="hero is-medium">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-4-desktop is-offset-4-desktop">
-                <div className="card">
-                  <div className="card-content">
-                    <form onSubmit={this.handleLogin}>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="text"
-                            placeholder="Shop Username"
-                            id="shop_username"
-                            value={this.state.shop_username}
-                            onChange={event => this.setState({shop_username: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="email"
-                            placeholder="Email"
-                            id="email"
-                            value={this.state.email}
-                            onChange={event => this.setState({email: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="password"
-                            placeholder="Password"
-                            id="password"
-                            value={this.state.password}
-                            onChange={event => this.setState({password: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="control has-text-centered">
-                        <button className="button is-main">Log In</button>
-                      </div>
-                      {
-                        this.props.showLoginError ? (
-                          <p id="error" className="help is-danger">
-                            Email or password is incorrect.
+      <div id="login">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns">
+                <div className="column is-4-desktop is-offset-4-desktop">
+                  <div className="card">
+                    <div className="card-content">
+                      <form onSubmit={this.handleLogin}>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="text"
+                              placeholder="Shop Username"
+                              id="shop_username"
+                              value={this.state.shop_username}
+                              onChange={event => this.setState({shop_username: event.target.value})}
+                              required
+                            />
                           </p>
-                        ) : null
-                      }
-                    </form>
+                        </div>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="email"
+                              placeholder="Email"
+                              id="email"
+                              value={this.state.email}
+                              onChange={event => this.setState({email: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="password"
+                              placeholder="Password"
+                              id="password"
+                              value={this.state.password}
+                              onChange={event => this.setState({password: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="control has-text-centered">
+                          <button className="button is-main">Log In</button>
+                        </div>
+                        {
+                          this.props.showLoginError ? (
+                            <p id="error" className="help is-danger">
+                              Email or password is incorrect.
+                            </p>
+                          ) : null
+                        }
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>     
     );
   };
 };
--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -42,142 +42,144 @@ class Signup extends React.Component {
 
   render () {
     return (
-      <section className="hero is-medium">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-6-desktop is-offset-3-desktop">
-                <div className="card">
-                  <div className="card-content">
-                    <form onSubmit={this.handleSignup}>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="text"
-                            placeholder="Shop Name"
-                            id="shop_name"
-                            value={this.state.shop_name}
-                            onChange={event => this.setState({shop_name: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="text"
-                            placeholder="Shop Username"
-                            id="shop_username"
-                            value={this.state.shop_username}
-                            onChange={event => this.setState({shop_username: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="email"
-                            placeholder="Shop Email"
-                            id="shop_email"
-                            value={this.state.shop_email}
-                            onChange={event => this.setState({shop_email: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field is-horizontal">
-                        <div className="field-body">
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                className="input"
-                                type="text"
-                                placeholder="First Name"
-                                id="first_name"
-                                value={this.state.first_name}
-                                onChange={event => this.setState({first_name: event.target.value})}
-                                required
-                              />
-                            </p>
-                          </div>
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                className="input"
-                                type="text"
-                                placeholder="Last Name"
-                                id="last_name"
-                                value={this.state.last_name}
-                                onChange={event => this.setState({last_name: event.target.value})}
-                                required
-                              />
-                            </p>
+      <div id="signup">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns">
+                <div className="column is-6-desktop is-offset-3-desktop">
+                  <div className="card">
+                    <div className="card-content">
+                      <form onSubmit={this.handleSignup}>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="text"
+                              placeholder="Shop Name"
+                              id="shop_name"
+                              value={this.state.shop_name}
+                              onChange={event => this.setState({shop_name: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="text"
+                              placeholder="Shop Username"
+                              id="shop_username"
+                              value={this.state.shop_username}
+                              onChange={event => this.setState({shop_username: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="email"
+                              placeholder="Shop Email"
+                              id="shop_email"
+                              value={this.state.shop_email}
+                              onChange={event => this.setState({shop_email: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="field is-horizontal">
+                          <div className="field-body">
+                            <div className="field">
+                              <p className="control">
+                                <input
+                                  className="input"
+                                  type="text"
+                                  placeholder="First Name"
+                                  id="first_name"
+                                  value={this.state.first_name}
+                                  onChange={event => this.setState({first_name: event.target.value})}
+                                  required
+                                />
+                              </p>
+                            </div>
+                            <div className="field">
+                              <p className="control">
+                                <input
+                                  className="input"
+                                  type="text"
+                                  placeholder="Last Name"
+                                  id="last_name"
+                                  value={this.state.last_name}
+                                  onChange={event => this.setState({last_name: event.target.value})}
+                                  required
+                                />
+                              </p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="field">
-                        <p className="control">
-                          <input
-                            className="input"
-                            type="email"
-                            placeholder="Email"
-                            id="email"
-                            value={this.state.email}
-                            onChange={event => this.setState({email: event.target.value})}
-                            required
-                          />
-                        </p>
-                      </div>
-                      <div className="field is-horizontal">
-                        <div className="field-body">
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                className={this.state.passwordClasses}
-                                type="password"
-                                placeholder="Password"
-                                id="password"
-                                value={this.state.password}
-                                onChange={event => this.setState({password: event.target.value})}
-                                required
-                              />
-                            </p>
-                          </div>
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                className={this.state.passwordClasses}
-                                type="password"
-                                placeholder="Verify Password"
-                                id="verify_password"
-                                value={this.state.verify_password}
-                                onChange={event => this.setState({verify_password: event.target.value})}
-                                required
-                              />
-                            </p>
+                        <div className="field">
+                          <p className="control">
+                            <input
+                              className="input"
+                              type="email"
+                              placeholder="Email"
+                              id="email"
+                              value={this.state.email}
+                              onChange={event => this.setState({email: event.target.value})}
+                              required
+                            />
+                          </p>
+                        </div>
+                        <div className="field is-horizontal">
+                          <div className="field-body">
+                            <div className="field">
+                              <p className="control">
+                                <input
+                                  className={this.state.passwordClasses}
+                                  type="password"
+                                  placeholder="Password"
+                                  id="password"
+                                  value={this.state.password}
+                                  onChange={event => this.setState({password: event.target.value})}
+                                  required
+                                />
+                              </p>
+                            </div>
+                            <div className="field">
+                              <p className="control">
+                                <input
+                                  className={this.state.passwordClasses}
+                                  type="password"
+                                  placeholder="Verify Password"
+                                  id="verify_password"
+                                  value={this.state.verify_password}
+                                  onChange={event => this.setState({verify_password: event.target.value})}
+                                  required
+                                />
+                              </p>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="control has-text-centered">
-                        <button className="button is-main">Sign Up</button>
-                      </div>
-                      {!this.state.isValid ? (
-                        <p id="error" className="help is-danger">
-                          Passwords do not match.
-                        </p>
-                      ) : null}
-                    </form>
+                        <div className="control has-text-centered">
+                          <button className="button is-main">Sign Up</button>
+                        </div>
+                        {!this.state.isValid ? (
+                          <p id="error" className="help is-danger">
+                            Passwords do not match.
+                          </p>
+                        ) : null}
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     );
   };
 };
