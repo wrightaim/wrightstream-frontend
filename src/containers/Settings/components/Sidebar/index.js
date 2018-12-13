7529d40204a2ed492f8d8ad09ce534ab755bbca8
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-12 19:02:09 -0800

    added icons to sidebar links in settings

--- src/containers/Settings/components/Sidebar/index.js
+++ src/containers/Settings/components/Sidebar/index.js
@@ -14,9 +14,9 @@ class Sidebar extends React.Component {
           Settings
         </p>
         <ul className="menu-list">
-          <li><Link to="/settings">Shop</Link></li>
-          <li><Link to="/settings/staff">Staff</Link></li>
-          <li><Link to="/settings/platforms">Platforms</Link></li>
+          <li><Link to="/settings"><span className="lnr lnr-store"></span>Shop</Link></li>
+          <li><Link to="/settings/staff"><span className="lnr lnr-users2"></span>Staff</Link></li>
+          <li><Link to="/settings/platforms"><span className="lnr lnr-layers"></span>Platforms</Link></li>
         </ul>
       </aside>
     );
