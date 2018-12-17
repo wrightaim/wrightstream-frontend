7b97c98c074265c96bee102072eb395f43111b21
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-16 20:17:26 -0800

    added redux functions to reset error messages for add, edit, archive staff

--- src/state/actions/shop.js
+++ src/state/actions/shop.js
@@ -9,10 +9,13 @@ export const GET_STAFFS = 'GET_STAFFS';
 export const GET_STAFFS_ARCHIVED = 'GET_STAFFS_ARCHIVED';
 export const ADD_STAFF_SUCCESS = 'ADD_STAFF_SUCCESS';
 export const ADD_STAFF_FAILURE = 'ADD_STAFF_FAILURE';
+export const ADD_STAFF_RESET = 'ADD_STAFF_RESET';
 export const EDIT_STAFF_SUCCESS = 'EDIT_STAFF_SUCCESS';
 export const EDIT_STAFF_FAILURE = 'EDIT_STAFF_FAILURE';
+export const EDIT_STAFF_RESET = 'EDIT_STAFF_RESET';
 export const ARCHIVE_STAFF_SUCCESS = 'ARCHIVE_STAFF_SUCCESS';
 export const ARCHIVE_STAFF_FAILURE = 'ARCHIVE_STAFF_FAILURE';
+export const ARCHIVE_STAFF_RESET = 'ARCHIVE_STAFF_RESET';
 export const RESTORE_STAFF = 'RESTORE_STAFF';
 export const GET_PLATFORMS = 'GET_PLATFORMS';
 
@@ -66,6 +69,12 @@ export const addStaff = staff => {
   };
 };
 
+export const addStaffReset = () => {
+  return dispatch => {
+    dispatch({type: ADD_STAFF_RESET});
+  };
+};
+
 export const editStaff = (staff, staff_id) => {
   return async dispatch => {
     try {
@@ -77,6 +86,12 @@ export const editStaff = (staff, staff_id) => {
   };
 };
 
+export const editStaffReset = () => {
+  return dispatch => {
+    dispatch({type: EDIT_STAFF_RESET});
+  };
+};
+
 export const archiveStaff = staff_id => {
   return async dispatch => {
     try {
@@ -85,6 +100,12 @@ export const archiveStaff = staff_id => {
     } catch (err) {
       dispatch({type: ARCHIVE_STAFF_FAILURE, payload: err});
     }
+  };
+};
+
+export const archiveStaffReset = () => {
+  return dispatch => {
+    dispatch({type: ARCHIVE_STAFF_RESET});
   };
 };
 
--- src/state/reducers/shop.js
+++ src/state/reducers/shop.js
@@ -7,10 +7,13 @@ import {
   GET_STAFFS,
   ADD_STAFF_SUCCESS,
   ADD_STAFF_FAILURE,
+  ADD_STAFF_RESET,
   EDIT_STAFF_SUCCESS,
   EDIT_STAFF_FAILURE,
+  EDIT_STAFF_RESET,
   ARCHIVE_STAFF_SUCCESS,
   ARCHIVE_STAFF_FAILURE,
+  ARCHIVE_STAFF_RESET,
   RESTORE_STAFF,
   GET_PLATFORMS
 } from '../actions/shop';
@@ -44,14 +47,20 @@ export default (state = initial, action) => {
       return {...state, staffs: action.payload, addStaffError: false};
     case ADD_STAFF_FAILURE:
       return {...state, addStaffError: true};
+    case ADD_STAFF_RESET:
+      return {...state, addStaffError: false};
     case EDIT_STAFF_SUCCESS:
       return {...state, staffs: action.payload, editStaffError: false};
     case EDIT_STAFF_FAILURE:
       return {...state, editStaffError: true};
+    case EDIT_STAFF_RESET:
+      return {...state, editStaffError: false};
     case ARCHIVE_STAFF_SUCCESS:
       return {...state, staffs: action.payload, archiveStaffError: false};
     case ARCHIVE_STAFF_FAILURE:
       return {...state, archiveStaffError: true};
+    case ARCHIVE_STAFF_RESET:
+      return {...state, archiveStaffError: false};
     case RESTORE_STAFF:
       return {...state, staffs: action.payload};
     case GET_PLATFORMS:
